3797be141dd88426e3a6d3cc0f7890d1a9d3b8a3
Jeremy Aw <jeremyinelysium@gmail.com>
2021-03-05 13:25:07 +0800

    Fix login functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Module1 from "./pages/Module1";
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Module1 from './pages/Module1';
 
 function App() {
+  const [credentials, setCredentials] = useState({});
+
   return (
     <div className='App'>
       <BrowserRouter>
         <Nav />
         <main>
           <Route path='/' exact component={Home} />
-          <Route path='/login' component={Login} />
+          <Route
+            path='/login'
+            component={() => <Login setCredentials={setCredentials} />}
+          />
           <Route path='/register' component={Register} />
           <Route path='/module1' component={Module1} />
           {/* add modules according */}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,10 +3,7 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -46,7 +43,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Login() {
+export default function Login({ setCredentials }) {
+  // const { setCredentials } = props;
   const classes = useStyles();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -67,7 +65,8 @@ export default function Login() {
       }
     ).then((response) => {
       response.json().then((body) => {
-        console.log(body);
+        setCredentials(body);
+        // console.log(body);
       });
     });
   };
@@ -107,10 +106,6 @@ export default function Login() {
             autoComplete='current-password'
             onChange={(e) => setPassword(e.target.value)}
           />
-          {/* <FormControlLabel
-            control={<Checkbox value='remember' color='primary' />}
-            label='Remember me'
-          /> */}
           <Button
             type='submit'
             fullWidth
@@ -120,18 +115,6 @@ export default function Login() {
           >
             Sign In
           </Button>
-          {/* <Grid container>
-            <Grid item xs>
-              <Link href='#' variant='body2'>
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href='#' variant='body2'>
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid> */}
         </form>
       </div>
       <Box mt={8}>
