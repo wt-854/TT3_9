ea79ca9d1d4135d5e6d19e04260b6bd39b203180
Jeremy Aw <jeremyinelysium@gmail.com>
2021-03-05 14:25:41 +0800

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React, { useState } from "react";
-import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Info from "./pages/Info";
-import History from "./pages/History";
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Info from './pages/Info';
+import History from './pages/History';
 
 function App() {
   const [credentials, setCredentials] = useState({});
 
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Nav />
         <main>
-          <Route path="/" exact component={Home} />
+          <Route path='/' exact component={Home} />
           <Route
-            path="/login"
+            path='/login'
             component={() => <Login setCredentials={setCredentials} />}
           />
           <Route
-            path="/viewinfo"
+            path='/viewinfo'
             component={() => <Info credentials={credentials} />}
           />
-          <Route path="/register" component={Register} />
-          <Route path="/history" component={History} />
+          <Route path='/register' component={Register} />
+          <Route path='/history' component={History} />
           {/* add modules according */}
         </main>
       </BrowserRouter>
